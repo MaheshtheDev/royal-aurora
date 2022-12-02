e0e90f948f723ee0bc5ca7b1574ddb4940664d7c
sailajauppalapati <sailajauppalapati99@gmail.com>
2022-12-02 22:20:54 +0530

    Sticky Header

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,9 +9,9 @@ export default function Footer() {
         <Image src="/logo_B.png" alt="secondlogo" height={85} width={125}/>
       </div>
       <div className="indent-5">
-        <div className="flex flex-col space-y-4 font-semibold font-Montserrat pb-8 text-xl"><a href="/">All Products</a></div>
+        <div className="flex flex-col space-y-4 font-semibold font-Montserrat pb-8 text-xl"><a href="#">All Products</a></div>
         <div className="flex flex-col space-y-4 pb-4 text-s">FAQs</div>
-        <div className="flex flex-col space-y-4 pb-4 text-s"><a href="#Contact us">Contact Us</a></div>
+        <div className="flex flex-col space-y-4 pb-4 text-s"><a href="#footer">Contact Us</a></div>
         <div className="flex flex-col space-y-4 pb-8 text-s">Our Promise</div>
       </div>
     </footer>
